3d964b5501f200aec16c55da2deb4da93e37d5c4
Kevin <pintokevin2002@hotmail.com>
2025-05-25 01:11:52 +0200

    add Tile class to represent a single tile on the game board

---
+++ src/main/java/Tile.java
@@ -0,0 +1,17 @@
+/**
+ * Tile class representing a single tile on the game board.
+ */
+public class Tile {
+    public int x;
+    public int y;
+
+    /**
+     * Constructor for Tile class.
+     * @param x The x-coordinate of the tile.
+     * @param y The y-coordinate of the tile.
+     */
+    Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
